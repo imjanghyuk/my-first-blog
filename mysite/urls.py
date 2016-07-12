71e2596e7d9d21ea980ea33bd876ab9583015fbe
JayLim <im.janghyuk@gmail.com>
2016-07-12 20:02:16 +0900

    authenticated users

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,6 @@ import django.contrib.auth.views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
